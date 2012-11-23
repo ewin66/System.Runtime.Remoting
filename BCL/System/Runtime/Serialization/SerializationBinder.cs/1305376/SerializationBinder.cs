cbeedbae7061c11c1368eab20e2222b7cccc6c41
wsky <wskyhx@gmail.com>
2012-11-23 11:04:02 +0800

    add remoting code

---
+++ BCL/System/Runtime/Serialization/SerializationBinder.cs/1305376/SerializationBinder.cs
@@ -0,0 +1,64 @@
+// ==++== 
+//
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// ==--== 
+/*============================================================
+** 
+** Interface: SerializationBinder 
+**
+** 
+** Purpose: The base class of serialization binders.
+**
+**
+===========================================================*/ 
+namespace System.Runtime.Serialization {
+    using System; 
+ 
+    [Serializable]
+[System.Runtime.InteropServices.ComVisible(true)] 
+    public abstract class SerializationBinder {
+
+        public virtual void BindToName(Type serializedType, out String assemblyName, out String typeName)
+        { 
+            assemblyName = null;
+            typeName = null; 
+        } 
+
+        public abstract Type BindToType(String assemblyName, String typeName); 
+    }
+}
+
+// File provided for Reference Use Only by Microsoft Corporation (c) 2007.
+// ==++== 
+//
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// ==--== 
+/*============================================================
+** 
+** Interface: SerializationBinder 
+**
+** 
+** Purpose: The base class of serialization binders.
+**
+**
+===========================================================*/ 
+namespace System.Runtime.Serialization {
+    using System; 
+ 
+    [Serializable]
+[System.Runtime.InteropServices.ComVisible(true)] 
+    public abstract class SerializationBinder {
+
+        public virtual void BindToName(Type serializedType, out String assemblyName, out String typeName)
+        { 
+            assemblyName = null;
+            typeName = null; 
+        } 
+
+        public abstract Type BindToType(String assemblyName, String typeName); 
+    }
+}
+
+// File provided for Reference Use Only by Microsoft Corporation (c) 2007.
---
+++ ManagedLibraries/Remoting/Services/RemotingService.cs/1305376/RemotingService.cs
@@ -0,0 +1,114 @@
+// ==++== 
+//
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// ==--== 
+namespace System.Runtime.Remoting.Services
+{ 
+    using System.Diagnostics; 
+    using System.Web;
+    using System.ComponentModel; 
+    using System.Web.SessionState;
+    using System.Security.Principal;
+    using System.Runtime.Remoting.Channels;
+ 
+    public class RemotingService : Component
+ 	{ 
+        public HttpApplicationState Application 
+		{
+            get { 
+                return Context.Application;
+            }
+        }
+ 
+        public HttpContext Context
+		{ 
+            get { 
+                HttpContext context = HttpContext.Current;
+                if (context == null) 
+                    throw new RemotingException(CoreChannel.GetResourceString("Remoting_HttpContextNotAvailable"));
+                return context;
+            }
+        } 
+
+        public HttpSessionState Session 
+		{ 
+            get {
+                return Context.Session; 
+            }
+        }
+
+        public HttpServerUtility Server 
+ 		{
+            get { 
+                return Context.Server; 
+            }
+        } 
+
+        public IPrincipal User
+		{
+            get { 
+                return Context.User;
+            } 
+        } 
+    }
+} 
+
+// File provided for Reference Use Only by Microsoft Corporation (c) 2007.
+// ==++== 
+//
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// ==--== 
+namespace System.Runtime.Remoting.Services
+{ 
+    using System.Diagnostics; 
+    using System.Web;
+    using System.ComponentModel; 
+    using System.Web.SessionState;
+    using System.Security.Principal;
+    using System.Runtime.Remoting.Channels;
+ 
+    public class RemotingService : Component
+ 	{ 
+        public HttpApplicationState Application 
+		{
+            get { 
+                return Context.Application;
+            }
+        }
+ 
+        public HttpContext Context
+		{ 
+            get { 
+                HttpContext context = HttpContext.Current;
+                if (context == null) 
+                    throw new RemotingException(CoreChannel.GetResourceString("Remoting_HttpContextNotAvailable"));
+                return context;
+            }
+        } 
+
+        public HttpSessionState Session 
+		{ 
+            get {
+                return Context.Session; 
+            }
+        }
+
+        public HttpServerUtility Server 
+ 		{
+            get { 
+                return Context.Server; 
+            }
+        } 
+
+        public IPrincipal User
+		{
+            get { 
+                return Context.User;
+            } 
+        } 
+    }
+} 
+
+// File provided for Reference Use Only by Microsoft Corporation (c) 2007.
